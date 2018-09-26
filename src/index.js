05263f43549e40d978df67d46f279ee3469e2d26
Vukasin Stojanovic <vultashin76@gmail.com>
2018-09-26 13:35:48 +0200

    Registration and Unique email, Validation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import auth from './routes/auth';
 import dotenv from 'dotenv';
-// import bcryptjs from 'bcryptjs';
 
+import auth from './routes/auth';
+import users from './routes/users';
+// import bcryptjs from 'bcryptjs';
 dotenv.config();
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json());
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
 app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 app.get('/*', (req, res) =>{
   res.sendFile(path.join(__dirname, 'index.html'));
